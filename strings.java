4b4090076285e962192cae455716bf9b54fbd48e
pawan raju koshti <pawan.koshti25@pccoepune.org>
2026-07-31 19:19:56 +0530

    Refactor main method to uncomment and organize code for clarity

--- strings.java
+++ strings.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class strings{
     public static void printLetters(String str){
         for(int i = 0; i<str.length(); i++){
@@ -21,24 +22,24 @@ public class strings{
         }
     }
     public static void main(String[] args){
-        // char arr[] = {'a','b','c','d'};
-        // String str = "abcd";
-        // String str2 = new String("xyz");
-        // //String are IMMUTABLE
-        // //that is strings are never changed
+        char arr[] = {'a','b','c','d'};
+         String str = "abcd";
+         String str2 = new String("xyz");
+        //String are IMMUTABLE
+         //that is strings are never changed
 
-        // Scanner sc = new Scanner(System.in);
-        // String name;
-        // name = sc.nextLine();
-        // System.out.println(name);
-        // String fullName="Tony Stark";
-        // System.out.println(fullName.length());
+         Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println(name);
+        String fullName="Tony Stark";
+        System.out.println(fullName.length());
         //concatenation
-        // String firstname = "Pawan";
-        // String lastname = "Koshti";
-        // String fullname = firstname + " " + lastname;
-        // System.out.println(fullname);
-        // printLetters(fullname);
+        String firstname = "Pawan";
+        String lastname = "Koshti";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
+        printLetters(fullname);
         palindrome("1223221");
     }
 }
